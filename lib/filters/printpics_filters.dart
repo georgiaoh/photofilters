4e2f3843059d95d2e549c0c6da150bdb76dc6f84
georgiaoh <g.o.hutchings@gmail.com>
2020-04-04 14:23:22 +0100

    New file with app specific filters

---
+++ lib/filters/printpics_filters.dart
@@ -0,0 +1,14 @@
+import 'dart:typed_data';
+import 'package:photofilters/filters/filters.dart';
+import 'preset_filters.dart';
+
+List<Filter> printPicsFilters = [
+  NoFilter(),
+  ClarendonFilter(),
+  JunoFilter(),
+  LudwigFilter(),
+  LarkFilter(),
+  GinghamFilter(),
+  LoFiFilter(),
+  AdenFilter(),
+];
